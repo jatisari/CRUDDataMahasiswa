47ffc632668cf52e4f5b81c6f7a2b80cce2078a2
Super Writer <jatisari.innovation@gmail.com>
2016-12-22 14:27:40 +0700

    menambahkan header

---
+++ app/src/main/java/net/agusharyanto/cruddatamahasiswa/GlobalVar.java
@@ -0,0 +1,9 @@
+package net.agusharyanto.cruddatamahasiswa;
+
+/**
+ * Created by agus on 12/22/16.
+ */
+public class GlobalVar {
+
+    public static final String IP_SERVER="http://192.168.10.132";
+}
